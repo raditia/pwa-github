72b7260d3b45fbd73bb699b750ace4327b0385d4
Raditia Madya <gustiraditia@gmail.com>
2018-11-30 01:14:24 +0700

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,7 @@
+const API_BASE_PATH = 'https://api.github.com/'
+
+export default {
+  api: {
+    users: API_BASE_PATH + 'users/'
+  }
+}
--- src/store.js
+++ src/store.js
@@ -1,16 +1,29 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import api from './api'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    user: {}
+  },
+  getters: {
+    user (state) {
+      return state.user
+    }
   },
   mutations: {
-
+    setUser (state, value) {
+      state.user = value
+    }
   },
   actions: {
-
+    getUser ({ commit }, { username, success, fail } = {}) {
+      api.getUser(username, (response) => {
+        commit('setUser', response.data)
+        if (success) success(response)
+      }, fail)
+    }
   }
 })
